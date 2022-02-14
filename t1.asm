edfaa351bbf001bca3e2e28585f2128d16b924f7
ru2501 <37130619+ru2501@users.noreply.github.com>
2022-02-15 01:04:02 +0800

    Add files via upload

---
+++ t1.asm
@@ -0,0 +1,25 @@
+assume cs:code
+a segment 
+	dw	1,2,3,4,5,6,7,8,0AH,0BH,0CH,0DH,0EH,0FH.0FFH
+a ends
+b segment 
+	dw 0,0,0,0,0,0,0,0
+b ends
+code segment
+start:	
+		mov ax,b
+		mov ss,ax
+		mov sp,16
+		
+		mov ax,a
+		mov ds,ax
+		mov bx,0
+		mov cx,8
+set:	push ds:[bx]
+		add bx,2
+		loop set
+	
+		mov ax,4C00H
+		int 21H
+code ends
+end start
